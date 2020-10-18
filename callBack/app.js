16ca8b735d5527c4e962d78714d1a5fe8797353a
ahmedusama25 <ahmed.usama25@hotmail.com>
2020-10-18 15:17:27 +0500

    without callback function

---
+++ callBack/app.js
@@ -0,0 +1,13 @@
+console.log("start")
+function info(email,password){
+    setTimeout(()=>{
+        return {useremail:email}
+    },5000)
+    
+}
+
+
+
+let newUser = info("ahmed.usama25@gmail.com",12345)
+
+console.log("finish")
